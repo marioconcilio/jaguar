852793d0ca0112b57c190ae92df51c133d8c0cd3
Mario Concilio <marioconcilio@gmail.com>
2018-09-18 18:03:00 -0300

    using defects4j examples

---
+++ br.usp.each.saeg.jaguar.example/run_jfreechart.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+# Script to run jaguar on defects4j JFreeChart
+#
+
+CP="$(defects4j export -p cp.test -w jfreechart)"
+PROJECT_DIR="jfreechart"
+CLASSES_DIR="build/"
+TESTS_DIR="build-tests/"
+JAGUAR_JAR="br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar"
+JAGUAR_MAIN_CLASS="br.usp.each.saeg.jaguar.core.cli.JaguarRunner"
+JACOCO_JAR="jacocoagent.jar"
+HEURISTIC="Ochiai"
+LOG_LEVEL="DEBUG" # ALL / TRACE / DEBUG / INFO / WARN / ERROR
+
+# CONTROL-FLOW
+
+java -javaagent:$JACOCO_JAR=output=tcpserver -cp .:$CP:$JAGUAR_JAR:$JACOCO_JAR \
+	$JAGUAR_MAIN_CLASS \
+		--heuristic "$HEURISTIC" \
+		--outputType H \
+		--output "control-flow.xml" \
+		--logLevel "$LOG_LEVEL" \
+		--projectDir "$PROJECT_DIR" \
+		--classesDir "$CLASSES_DIR" \
+		--testsDir "$TESTS_DIR"
+
+# DATA-FLOW
+
+java -javaagent:$JACOCO_JAR=output=tcpserver,dataflow=true -cp .:$CP:$JAGUAR_JAR:$JACOCO_JAR \
+	$JAGUAR_MAIN_CLASS \
+		--dataflow \
+		--heuristic "$HEURISTIC" \
+		--outputType H \
+		--output "data-flow.xml" \
+		--logLevel "$LOG_LEVEL" \
+		--projectDir "$PROJECT_DIR" \
+		--classesDir "$CLASSES_DIR" \
+		--testsDir "$TESTS_DIR"
---
+++ br.usp.each.saeg.jaguar.example/run_mockito.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 
+# Script to run jaguar on defects4j Mockito
+#
+
+CP="$(defects4j export -p cp.test -w mockito)"
+PROJECT_DIR="mockito/"
+CLASSES_DIR="build/classes/main/"
+TESTS_DIR="build/classes/test/"
+JAGUAR_JAR="br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar"
+JAGUAR_MAIN_CLASS="br.usp.each.saeg.jaguar.core.cli.JaguarRunner"
+JACOCO_JAR="jacocoagent.jar"
+HEURISTIC="Ochiai"
+LOG_LEVEL="DEBUG" # ALL / TRACE / DEBUG / INFO / WARN / ERROR
+
+# CONTROL-FLOW
+
+java -javaagent:$JACOCO_JAR=output=tcpserver -cp .:$CP:$JAGUAR_JAR:$JACOCO_JAR \
+	$JAGUAR_MAIN_CLASS \
+		--heuristic "$HEURISTIC" \
+		--outputType H \
+		--output "control-flow.xml" \
+		--logLevel "$LOG_LEVEL" \
+		--projectDir "$PROJECT_DIR" \
+		--classesDir "$CLASSES_DIR" \
+		--testsDir "$TESTS_DIR" \
+		--testSuite "$TEST_SUITE"
+
+# DATA-FLOW
+
+java -javaagent:$JACOCO_JAR=output=tcpserver,dataflow=true -cp .:$CP:$JAGUAR_JAR:$JACOCO_JAR \
+	$JAGUAR_MAIN_CLASS \
+		--dataflow \
+		--heuristic "$HEURISTIC" \
+		--outputType H \
+		--output "data-flow.xml" \
+		--logLevel "$LOG_LEVEL" \
+		--projectDir "$PROJECT_DIR" \
+		--classesDir "$CLASSES_DIR" \
+		--testsDir "$TESTS_DIR" \
+		--testSuite "$TEST_SUITE"
